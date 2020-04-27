177626227a6f4fdcac3f6c0ba6692cbc8952eec4
hassibayub <hassibayub@gmail.com>
2020-04-27 22:43:47 +0500

    all pages scraped auto complete

--- glassesshop/spiders/items.py
+++ glassesshop/spiders/items.py
@@ -7,9 +7,28 @@ class ItemsSpider(scrapy.Spider):
     allowed_domains = ['www.glassesshop.com']
     start_urls = ['https://www.glassesshop.com/bestsellers']
 
-    def parse(self, response):
-        
-        for items in response.xpath("//div[@class='prlist row']"):
 
-            container = items.xpath(".//div[@class='col-sm-6 col-md-4 m-p-product']").get()
-            print(container)
+    # com = div[@class='col-sm-6 col-md-4 m-p-product']/div[@class="pimg default-image-front"]/a/img[@src]
+
+    def parse(self, response):
+      
+        for items in response.xpath("//div[@class='col-sm-6 col-md-4 m-p-product']"):
+
+            item_url = items.xpath(".//div[@class='pimg default-image-front']/a/@href").get()
+            img_url = items.xpath(".//div[@class='pimg default-image-front']/a/img").get()
+            item_name = items.xpath(".//div[@class='row']/p/a/text()").get()
+            price = items.xpath(".//div[@class='row']/div/span/text()").get()
+
+
+            yield {
+                "item_url" : item_url,
+                "img_url" : img_url,
+                "item_name": item_name,
+                "price": price
+            } 
+        
+        next_page = response.xpath("(//li[@class='page-item'])[4]/a/@href").get()
+
+        if next_page:
+            yield scrapy.Request(url=next_page, callback= self.parse)
+            
